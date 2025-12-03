de833e742562f09ab7f27d8e0ebb5e503007fd0a
kingyj00 <onshopyj@gmail.com>
2025-12-03 15:10:27 +0900

    [Feat] 메인페이지 정렬 및 연동 성공

--- src/app/page.js
+++ src/app/page.js
@@ -2,28 +2,34 @@ export default function Page() {
   return (
     <div className="bg-[url(/rectangle-1.png)] bg-[100%_100%] w-full min-w-[1440px] min-h-[1600px] relative">
 
-      <header className="absolute top-[52px] left-[55px] right-[55px]">
-        <img
-          className="w-[1330px] h-[3px]"
-          alt=""
-          src="/line-1.svg"
-        />
+      {/* HEADER */}
+      <header className="w-full flex justify-center mt-[52px]">
+        <div className="w-[1330px]">
 
-        <nav className="flex justify-between items-start mt-[17px]">
-          <div className="text-[#c03f3f] text-xl whitespace-nowrap">
-            SHARE CULTURE
-          </div>
+          <img
+            className="w-full h-[3px]"
+            alt=""
+            src="/line-1.svg"
+          />
 
-          <p className="text-[#c03f3f] text-xl whitespace-nowrap">
-            Food, House, Event, Public Transper, Everything
-          </p>
+          <nav className="flex justify-between items-start mt-[17px]">
+            <div className="text-[#c03f3f] text-xl whitespace-nowrap">
+              SHARE CULTURE
+            </div>
 
-          <div className="text-[#c03f3f] text-xl whitespace-nowrap">
-            SHARE LIFE
-          </div>
-        </nav>
+            <p className="text-[#c03f3f] text-xl whitespace-nowrap">
+              Food, House, Event, Public Transper, Everything
+            </p>
+
+            <div className="text-[#c03f3f] text-xl whitespace-nowrap">
+              SHARE LIFE
+            </div>
+          </nav>
+
+        </div>
       </header>
 
+      {/* MAIN IMAGE */}
       <main className="absolute top-[195px] left-[430px] w-[557px] h-[431px]">
         <img
           className="w-full h-full"
@@ -32,12 +38,14 @@ export default function Page() {
         />
       </main>
 
+      {/* LOGIN */}
       <aside className="absolute top-[300px] left-[1218px] w-[17px]">
         <button className="text-[#c03f3f] text-4xl text-center leading-[normal]">
           L<br />O<br />G<br />I<br />N
         </button>
       </aside>
 
+      {/* ARROW */}
       <div className="absolute top-[373px] left-[1280px] w-[110px] h-[74px]">
         <img
           className="w-full h-full cursor-pointer hover:opacity-80 transition-opacity"
@@ -46,17 +54,24 @@ export default function Page() {
         />
       </div>
 
-      <footer className="absolute top-[766px] left-[55px] right-[55px]">
-        <img
-          className="absolute top-[31px] left-0 w-[1330px] h-[3px]"
-          alt=""
-          src="/line-2.svg"
-        />
+      {/* FOOTER (line2를 header처럼 변경) */}
+      <footer className="w-full flex justify-center absolute top-[766px]">
+        <div className="w-[1330px]">
 
-        <p className="absolute top-0 left-[158px] text-[#c03f3f] text-base whitespace-nowrap">
-          Community service that shares and shares everything together,
-          including culture, transportation, and food in Seoul.
-        </p>
+          {/* 설명 문구 */}
+          <p className="text-center text-[#c03f3f] text-base whitespace-nowrap mb-[30px]">
+            Community service that shares and shares everything together,
+            including culture, transportation, and food in Seoul.
+          </p>
+
+          {/* 밑줄 line2 */}
+          <img
+            className="w-full h-[3px]"
+            alt=""
+            src="/line-2.svg"
+          />
+
+        </div>
       </footer>
 
     </div>
